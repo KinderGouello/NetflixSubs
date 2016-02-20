ac32a804da9de0bf7ba1cf8ad4dedb046dd60a16
Rémy Gouello <rigouello@gmail.com>
2016-02-20 13:35:18 +0100

    Maj

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 $(function() {
 
-	// var $urlApi = 'http://remygouello.fr/ApiSubtitles/site/';
-	var $urlApi = 'http://192.168.99.100/ApiSubtitles/site/',
+	var $urlApi = 'http://remygouello.fr/ApiSubtitles/site/',
+	// var $urlApi = 'http://192.168.99.100/ApiSubtitles/site/',
 		$key = '',
 		$language = window.navigator.userLanguage || window.navigator.language,
 		$title,
+		$season = '',
+		$episode = '',
 		$explodes;
 
 	function getLogin() {
@@ -36,8 +38,12 @@ $(function() {
 
 									// Si la personne est loggée, alors on lui propose de télécharger les soustitres
 									$key = data.key;
-									// getSubtitles($title.text(), $explodes[1], $explodes[3], $language);
-									getSubtitles($title.text(), parseInt($explodes[1].replace(':', '')), parseInt($explodes[3]), $language);
+									
+									if ($explodes.length !== 0) {
+										$season = parseInt($explodes[1].replace(':', ''));
+										$episode = parseInt($explodes[3]);
+									}
+									getSubtitles($title.text(), $season, $episode, $language);
 								}
 							}
 						);
@@ -46,14 +52,20 @@ $(function() {
 			} else {
 				// Si la personne est loggée, alors on lui propose de télécharger les soustitres
 				$key = data.value;
-				// getSubtitles($title.text(), $explodes[1], $explodes[3], $language);
-				getSubtitles($title.text(), parseInt($explodes[1].replace(':', '')), parseInt($explodes[3]), $language);
+
+				if ($explodes.length > 1) {
+					$season = parseInt($explodes[1].replace(':', ''));
+					$episode = parseInt($explodes[3]);
+				}
+				getSubtitles($title.text(), $season, $episode, $language);
 			}
 		});
 	}
 
 	function getSubtitles($title, $season, $episode, $language) {
-		$('#netflixSubs').html('').load('html/loader.html');
+		$('#netflixSubs').html('').load('html/loader.html', function() {
+			$('#messageLoader').text('Recovery subtitles...');
+		});
 
 		$.get($urlApi + 'soustitres/download',
 			{
@@ -76,7 +88,9 @@ $(function() {
 	}
 
 	function getListSubtitles($title, $season, $episode, $language) {
-		$('#netflixSubs').html('').load('html/loader.html');
+		$('#netflixSubs').html('').load('html/loader.html', function() {
+			$('#messageLoader').text('No subtitles was already downloaded, recovery attempt subtitles lists...');
+		});
 
 		// Si pas de soustitres enregistrés en BDD
 		$.get($urlApi + 'soustitres/liste',
@@ -91,32 +105,41 @@ $(function() {
 				data = $.parseJSON(data);
 
 				if (data.success === 1) {
-					$('#netflixSubs').html('<form id="netflixSubsChooseSubtitle" class="form-horizontal"><select class="form-control" name="subtitles" id="netflixSubsSubtitles"></select><button type="submit" class="btn btn-default">Choose</button></form>');
-					$.each(data.liste, function(index, val) {
-						 $('#netflixSubs select').append('<option value="' + val.IDSubtitleFile + '">' + val.SubFileName + '</option>');
-					});
+					$('#netflixSubs').html('').load('html/list.html', function() {
+						$.each(data.liste, function(index, val) {
+							 $('#netflixSubsSubtitles').append('<option value="' + val.IDSubtitleFile + '">' + val.SubFileName + '</option>');
+						});
 
-					$('#netflixSubsChooseSubtitle').off('submit').on('submit', function(event) {
-						event.preventDefault();
+						$('#netflixSubsChooseSubtitle').off('submit').on('submit', function(event) {
+							event.preventDefault();
+							$subtitlesSelected = $('#netflixSubsSubtitles').find(':selected').val();
 
-						$.get($urlApi + 'soustitres/downloadById',
-							{
-								title: $title,
-								season: $season,
-								episode: $episode,
-								idserie: data.idserie,
-								language: $language,
-								idsubtitle: $('#netflixSubsSubtitles option:selected').val(),
-								key: $key
-							},
-							function(data) {
+							$('#netflixSubs').html('').load('html/loader.html', function() {
+								$('#messageLoader').text('Recovery subtitles...');
+							});
+
+							$.get($urlApi + 'soustitres/downloadById',
+								{
+									title: $title,
+									season: $season,
+									episode: $episode,
+									idserie: data.idserie,
+									language: $language,
+									idsubtitle: $subtitlesSelected,
+									key: $key
+								}
+							)
+							.done(function(data) {
 								data = $.parseJSON(data);
 
 								if (data.success === 1 && data.subtitles !== '') {
 									downloadPage(data.subtitles, data.name);
-								}	
-							}
-						);
+								}
+							})
+							.fail(function(data) {
+								getListSubtitles($title, $season, $episode, $language);
+							});
+						});
 					});
 				}
 			}
@@ -129,6 +152,8 @@ $(function() {
 
 	function downloadPage(subtitles, name) {
 		$('#netflixSubs').html('').load('html/download.html', function() {
+			$('#subtitlesFilename').text(name);
+
 			$('#DownloadSubtitles').off('click').on('click', function(event) {
 				chrome.downloads.download({
 				  url: 'data:,' + subtitles,
@@ -155,7 +180,16 @@ $(function() {
 			}
 		});
 
-		$('#netflixSubs').html('').load('html/loader.html');
+		if ($title === undefined && $explodes === undefined) {
+			$('#netflixSubs').html('').load('html/error.html', function() {
+				$('.alert').html('<strong>Error,</strong> you’re not in a Netflix video or it isn’t loaded.');
+			});
+			return false;
+		}
+
+		$('#netflixSubs').html('').load('html/loader.html', function() {
+			$('#messageLoader').text('Loading...');
+		});
 
 		getLogin();
 	  }
